f7e3bc89333bb459f995e06355c8cd0f889b111d
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-06 17:11:10 +0530

    Work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Work from './components/work'
+import Work from './components/Work'
 import Stripes from './components/Stripes'
 import Products from './components/Products'
 import Marquees from './components/marquees'
