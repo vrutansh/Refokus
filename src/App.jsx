bdcfff1b60fbb292a1ac4ec9b76e8589bbbd1aa3
Vrutansh Patel <vrutanshpatel01@gmail.com>
2024-03-06 17:13:58 +0530

    edit few typos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 import Work from './components/Work'
 import Stripes from './components/Stripes'
 import Products from './components/Products'
-import Marquees from './components/marquees'
+import Marquees from './components/Marquees'
 import Cards from './components/Cards'
 import Footer from './components/Footer'
 
